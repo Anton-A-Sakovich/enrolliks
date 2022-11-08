a5f6aae530ae4219a96e21bba0cf8c5c435bd03b
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-08 17:49:42 +0300

    Add error label to create person form

---
+++ Enrolliks.Web/React/src/people/createPersonExpander.js
@@ -0,0 +1,75 @@
+const React = require('react');
+const ErrorLabel = require('../errorLabel');
+const person = require('./person');
+
+module.exports = class CreatePersonExpander extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editing: false,
+        };
+    }
+
+    render() {
+        return this.state.editing
+            ? <CreatePersonForm
+                onCancel={() => this.setState({editing: false})}
+                onComplete={() => this.setState({editing: false})} />
+            : <button onClick={() => this.setState({editing: true})}>Create</button>
+    }
+}
+
+class CreatePersonForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            nameError: '',
+            showNameError: false,
+        };
+    }
+
+    render() {
+        return (
+            <form onSubmit={event => this.onSubmit(event)}>
+                <div className="panel">
+                    <div className='form-row'>
+                        <label className='form-label' htmlFor="name">Name</label>
+                        <input className='form-input' type="text" id="name" name="name" value={this.state.name} onChange={event => this.handleNameChange(event)} />
+                        <ErrorLabel className='form-label form-label-error' htmlFor='name' showError={this.state.showNameError}>{this.state.nameError}</ErrorLabel>
+                    </div>
+                    <div>
+                        <button onClick={() => this.handleSubmitClick()}>Create</button>
+                        <button onClick={() => this.handleCancelClick()}>Cancel</button>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        return false;
+    }
+
+    handleNameChange(event) {
+        const name = event.target.value;
+        const nameError = person.validateName(name);
+
+        this.setState({
+            name: name,
+            nameError: nameError,
+            showNameError: true,
+        });
+    }
+
+    handleCancelClick() {
+        this.props.onCancel();
+    }
+
+    handleSubmitClick() {
+        this.props.onComplete();
+    }
+}
--- Enrolliks.Web/React/src/people/peoplePage.js
+++ Enrolliks.Web/React/src/people/peoplePage.js
@@ -1,60 +1,49 @@
 const React = require('react');
-const CreatePersonForm = require('./createPersonForm');
+const CreatePersonExpander = require('./createPersonExpander');
 const PersonRow = require('./personRow');
 
-const success = 0;
-const failure = 1;
+const getAllPeopleResultType = {
+    success: 0,
+    failure: 1,
+};
 
 module.exports = class PeoplePage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        let content;
-        const args = this.props.args;
-        if (args === null || !('type' in args) || args.type === failure)
-            content = <ErrorContent />;
-        else if (args.type === success)
-            content = args.people.length === 0
-                ? <EmptyContent />
-                : <PeopleContent people={args.people} />;
-        else
-            content = <ErrorContent />;
-
-        this.state = {
-            content: content,
-        };
-    }
-
     render() {
+        const args = this.props.args;
+        const content = (args === null || !('type' in args) || args.type !== getAllPeopleResultType.success)
+            ? <ErrorMessage />
+            : args.people.length === 0
+                ? <NoPeopleMessage />
+                : <PeopleTable people={args.people} />;
+
         return (
             <div>
                 <div>
-                    <CreatePersonForm />
+                    <CreatePersonExpander />
                 </div>
-                <div>
-                    {this.state.content}
+                <div className='panel'>
+                    {content}
                 </div>
             </div>
         );
     }
 }
 
-class EmptyContent extends React.Component {
+class NoPeopleMessage extends React.Component {
     render() {
         return <p>No people found.</p>;
     }
 }
 
-class ErrorContent extends React.Component {
+class ErrorMessage extends React.Component {
     render() {
         return <p>Error.</p>;
     }
 }
 
-class PeopleContent extends React.Component {
+class PeopleTable extends React.Component {
     render() {
         return (
-        <div className='panel'>
             <table>
                 <thead>
                     <tr>
@@ -65,6 +54,6 @@ class PeopleContent extends React.Component {
                     {this.props.people.map(person => <PersonRow key={person.name} person={person} />)}
                 </tbody>
             </table>
-        </div>);
+        );
     }
 }
