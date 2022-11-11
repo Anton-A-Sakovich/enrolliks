ab0b9a6c2523cde138c9a909eac3be1d4853eede
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-11 19:55:58 +0300

    Remove `person.js`
    It has been renamed into `personUtils.js`, just forgot to stage this
    change.

--- Enrolliks.Web/React/src/people/person.js
+++
@@ -1,9 +0,0 @@
-exports.validateName = name => {
-    if (!name || name.length < 3)
-        return 'The name is too short';
-
-    if (name.length > 128)
-        return 'The name is too long';
-
-    return '';
-};
