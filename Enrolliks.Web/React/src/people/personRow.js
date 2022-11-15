89421c538fa5efce5e96ccbe47fcf75be53d5d4f
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-16 00:33:25 +0300

    Update `PersonRow`
    Switch from `RemoveButton` to `ImageButton`.

--- Enrolliks.Web/React/src/people/personRow.js
+++ Enrolliks.Web/React/src/people/personRow.js
@@ -1,10 +1,17 @@
 ﻿const React = require('react');
-const RemoveButton = require('../removeButton');
+const ImageButton = require('../imageButton');
 
 module.exports = function PersonRow(props) {
     return (
         <tr>
-            <td>{props.person.name} <RemoveButton /></td>
+            <td>
+                {props.person.name}
+                <ImageButton
+                    className='remove-button'
+                    src='/img/circle-xmark-regular.svg'
+                    hoverSrc='/img/circle-xmark-solid.svg'
+                    downSrc='/img/circle-xmark-solid-gray.svg' />
+            </td>
         </tr>
     );
 };
