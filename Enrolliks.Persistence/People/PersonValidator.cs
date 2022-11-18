1a1253bebf5186a7f1b9915df2ad72578042d3d7
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-18 13:54:33 +0300

    Implement `PersonValidator`

--- Enrolliks.Persistence/People/PersonValidator.cs
+++ Enrolliks.Persistence/People/PersonValidator.cs
@@ -1,16 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Enrolliks.Persistence.People
 {
     internal class PersonValidator : IPersonValidator
     {
+        private const int _minNameLength = 3;
+        private const int _maxNameLength = 128;
+
+        private static readonly Regex _disallowedLetterRegex = new(@"[^A-Za-z\ ]", RegexOptions.Compiled);
+
         public PersonValidationErrors? Validate(Person person)
         {
-            throw new NotImplementedException();
+            if (person is null) throw new ArgumentNullException(nameof(person));
+
+            IPersonNameValidationError? nameError = person.Name switch
+            {
+                var s when string.IsNullOrWhiteSpace(s) => new IPersonNameValidationError.Empty(),
+
+                string s when s.Length < _minNameLength => new IPersonNameValidationError.TooShort(_minNameLength),
+                string s when s.Length > _maxNameLength => new IPersonNameValidationError.TooLong(_maxNameLength),
+
+                string s when char.IsWhiteSpace(s, 0) => new IPersonNameValidationError.StartsWithSpace(),
+
+                string s when _disallowedLetterRegex.Match(s) is Match { Success: true } match
+                    => new IPersonNameValidationError.DisallowedLetter(s.Substring(match.Index, match.Length)),
+
+                _ => null,
+            };
+
+            if (nameError is not null)
+                return new PersonValidationErrors { Name = nameError };
+
+            return null;
         }
     }
 }
