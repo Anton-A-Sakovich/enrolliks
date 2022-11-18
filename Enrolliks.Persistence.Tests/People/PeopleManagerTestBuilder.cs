8ffaaa542097a6e2d775a96192b9dd537e1ec33b
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-18 20:54:18 +0300

    Add `PeopleManagerTestBuilder`

---
+++ Enrolliks.Persistence.Tests/People/PeopleManagerTestBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Enrolliks.Persistence.People;
+using NUnit.Framework;
+
+namespace Enrolliks.Persistence.Tests.People
+{
+    internal class PeopleManagerTestBuilder
+    {
+        public MockBuilder<IPeopleRepository> RepositoryBuilder { get; } = new();
+        public MockBuilder<IPersonValidator> ValidatorBuilder { get; } = new();
+        public AssertionsBuilder<PeopleManager> AssertionsBuilder { get; } = new();
+
+        public void Test()
+        {
+            var (repository, verifyRepository) = RepositoryBuilder.Build();
+            var (validator, verifyValidator) = ValidatorBuilder.Build();
+            var manager = new PeopleManager(repository, validator);
+
+            Assert.Multiple(() =>
+            {
+                AssertionsBuilder.Test(manager);
+                Assert.That(verifyRepository, Throws.Nothing);
+                Assert.That(verifyValidator, Throws.Nothing);
+            });
+        }
+    }
+}
