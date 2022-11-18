4d2606549bf9ce1926faeabf14e8ee42b3b50ac7
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-18 20:51:23 +0300

    Add `AssertionsBuilder<T>`

---
+++ Enrolliks.Persistence.Tests/AssertionsBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+
+namespace Enrolliks.Persistence.Tests
+{
+    internal class AssertionsBuilder<T> where T : class
+    {
+        private readonly IList<Action<T>> _assertions = new List<Action<T>>();
+
+        public AssertionsBuilder<T> Assert<TResult>(Func<T, TResult> action, IResolveConstraint constraint)
+        {
+            if (action is null) throw new ArgumentNullException(nameof(action));
+            if (constraint is null) throw new ArgumentNullException(nameof(constraint));
+
+            _assertions.Add(instance => NUnit.Framework.Assert.That(() => action(instance), constraint));
+
+            return this;
+        }
+
+        public void Test(T instance)
+        {
+            NUnit.Framework.Assert.Multiple(() =>
+            {
+                foreach (var assertion in _assertions)
+                    assertion(instance);
+            });
+        }
+    }
+}
