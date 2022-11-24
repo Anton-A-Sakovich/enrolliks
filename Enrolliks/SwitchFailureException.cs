35b541c0b2c92ff5e89e6bcbe6d3c18f3cb838a2
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-24 17:58:33 +0300

    Move `SwitchFailureException` to Enrolliks project
    I guess pretty match everything will need it.

--- Enrolliks/SwitchFailureException.cs
+++ Enrolliks/SwitchFailureException.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Runtime.Serialization;
 
-namespace Enrolliks.Web.Controllers
+namespace Enrolliks
 {
     /// <summary>
     /// The exception that is thrown when none of the <c>switch</c> patterns were able to match the <c>switch</c> expression.
     /// </summary>
-    internal class SwitchFailureException : ApplicationException
+    public class SwitchFailureException : ApplicationException
     {
         public SwitchFailureException() : base("The switch cases were incomplete.")
         {
