c488af35d3f6e096cfd6366d0727b69327f826c9
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-25 14:09:01 +0300

    Update `peoplePage.js`
    Restructure the page to use a hooked function component and React
    Router loader data.

--- Enrolliks.React/src/people/peoplePage.js
+++ Enrolliks.React/src/people/peoplePage.js
@@ -1,61 +1,52 @@
 const React = require('react');
 const CreatePersonExpander = require('./createPersonExpander');
 const PersonRow = require('./personRow');
+const { getPeopleResultType } = require('./personEndpoints');
+const { useLoaderData } = require('react-router-dom');
+const { useState, useEffect } = require('react');
 
-const getAllPeopleResultType = {
-    success: 'Success',
-    failure: 'RepositoryFailure',
-};
+module.exports = function PeoplePage() {
+    const getPeopleResult = useLoaderData();
 
-module.exports = class PeoplePage extends React.Component {
-    constructor(props) {
-        super(props);
+    const [isError, setIsError] = useState(true);
+    const [people, setPeople] = useState([]);
 
-        const args = this.props.args;
-        if (args === null || !('tag' in args) || args.tag !== getAllPeopleResultType.success) {
-            this.state = {
-                people: [],
-                isError: true,
-            };
+    useEffect(() => {
+        if (getPeopleResult.tag === getPeopleResultType.success) {
+            setIsError(false);
+            setPeople(getPeopleResult.people);
         } else {
-            this.state = {
-                people: args.value.people,
-                isError: false,
-            };
+            setIsError(true);
+            setPeople([]);
         }
-    }
+    }, [getPeopleResult]);
 
-    render() {
-        const content = this.state.isError
-            ? <p>Error.</p>
-            : this.state.people.length === 0
-                ? <p>No people found.</p>
-                : <PeopleTable
-                    people={this.state.people}
-                    onPersonRemoved={this.handlePersonRemoved} />;
-
-        return (
-            <div>
-                <div>
-                    <CreatePersonExpander />
-                </div>
-                <div className='panel'>
-                    {content}
-                </div>
-            </div>
-        );
-    }
-
-    handlePersonRemoved = index => {
-        this.setState(previousState => {
-            const peopleCopy = previousState.people.slice();
+    const handlePersonRemoved = index => {
+        setPeople(previousPeople => {
+            const peopleCopy = previousPeople.slice();
             peopleCopy.splice(index, 1)
-            
-            return {
-                people: peopleCopy,
-            };
+            return peopleCopy;
         });
     }
+
+    const content = isError
+            ? <p>Error.</p>
+            : people.length === 0
+                ? <p>No people found.</p>
+                : <PeopleTable
+                    people={people}
+                    onPersonRemoved={handlePersonRemoved} />;
+
+    return (
+        <div>
+            <div>
+                <CreatePersonExpander />
+            </div>
+            <div className='panel'>
+                {content}
+            </div>
+        </div>
+    );
 }
 
 class PeopleTable extends React.Component {
