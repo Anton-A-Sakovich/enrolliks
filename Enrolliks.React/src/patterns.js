665d5bc9b407c0816241139410bebebb20c33646
Anton Sakovich <anton.sakovich@softteco.com>
2022-11-30 21:11:10 +0300

    Add `patterns.js`
    Add some pattern matching functions for checking response bodies.

---
+++ Enrolliks.React/src/patterns.js
@@ -0,0 +1,113 @@
+// <TerminalPatterns>
+
+module.exports.isAny = isAny;
+function isAny() {
+    return true;
+}
+
+module.exports.isConstant = isConstant;
+function isConstant(constant) {
+    return function isConstant$(value) {
+        return value === constant;
+    }
+}
+
+module.exports.isBoolean = isBoolean;
+function isBoolean(value) {
+    return typeof(value) === 'boolean';
+}
+
+module.exports.isNumber = isNumber;
+function isNumber(value) {
+    return typeof(value) === 'number';
+}
+
+module.exports.isString = isString;
+function isString(value) {
+    return typeof(value) === 'string';
+}
+
+// </TerminalPatterns>
+// <CombinatorPatterns>
+
+module.exports.not = not;
+function not(pattern) {
+    return function not$(value) {
+        return !pattern(value);
+    }
+}
+
+module.exports.and = and;
+function and(...patterns) {
+    return function and$(value) {
+        for (const pattern of patterns) {
+            if (!pattern(value)) return false;
+        }
+
+        return true;
+    };
+}
+
+module.exports.or = or;
+function or(...patterns) {
+    return function or$(value) {
+        for (const pattern of patterns) {
+            if (pattern(value)) return true;
+        }
+
+        return false;
+    };
+}
+
+// </CombinatorPatterns>
+// <StructuralPatterns>
+
+module.exports.hasProperty = hasProperty;
+function hasProperty(name, pattern) {
+    return function hasProperty$(value) {
+        return (name in value) && pattern(value[name]);
+    }
+}
+
+module.exports.isObject = isObject;
+function isObject(...patterns) {
+    return function isObject$(value) {
+        return !!value && typeof(value) === 'object' && !Array.isArray(value) && and(...patterns)(value)
+    };
+}
+
+module.exports.repeated = repeated;
+function repeated(pattern, min) {
+    return function repeated$(values) {
+        if (values.length < min) {
+            return false;
+        }
+
+        for (const value of values) {
+            if (!pattern(value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports.isArray = isArray;
+function isArray(...patterns) {
+    return function isArray$(value) {
+        if (!Array.isArray(value)) {
+            return false;
+        }
+
+        for (const pattern of patterns) {
+            if (!pattern(value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// </StructuralPatterns>
